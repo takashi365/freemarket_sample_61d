1d5bc0e4427b973f5c8351ac7e6cb3698de46a0f
160ayumi <160ayumi@gmail.com>
2019-11-11 13:22:12 +0900

    ignoreテスト

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_11_09_084802) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -34,9 +33,6 @@ ActiveRecord::Schema.define(version: 2019_11_09_084802) do
     t.datetime "updated_at", null: false
   end
 
-
-ActiveRecord::Schema.define(version: 2019_11_07_130322) do
-
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "image_url"
     t.bigint "item_id"
@@ -45,8 +41,6 @@ ActiveRecord::Schema.define(version: 2019_11_07_130322) do
     t.index ["item_id"], name: "index_images_on_item_id"
   end
 
-ActiveRecord::Schema.define(version: 2019_11_07_145914) do
-  
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.text "description"
@@ -80,9 +74,6 @@ ActiveRecord::Schema.define(version: 2019_11_07_145914) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-
-  add_foreign_key "images", "items"
-
   add_foreign_key "addresses", "users"
-
+  add_foreign_key "images", "items"
 end
