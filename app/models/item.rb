8e9a4f71a98f1aee27a385ba5f862cbff6923392
suzuki-nori <www.rino19@gmail.com>
2019-11-14 19:33:49 +0900

    Revert "revert.commit"
    This reverts commit cd48b1c6e32dab46d4bac02f81d316e44dc56cd9.

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,7 +12,6 @@ class Item < ApplicationRecord
   validates :prefecture, presence: true
   validates :arrival_date, presence: true
 
-
   enum status:{"---":0,
     新品・未使用:1,未使用に近い:2,目立った傷や汚れなし:3,やや傷や汚れあり:4, 汚れや傷あり:5,
     全体的に状態が悪い:6
---
+++ db/migrate/20191114042625_add_column_items.rb
@@ -0,0 +1,6 @@
+class AddColumnItems < ActiveRecord::Migration[5.2]
+  def change
+    add_column :items, :seller_id, :integer
+    add_column :items, :buyer_id, :integer
+  end
+end
