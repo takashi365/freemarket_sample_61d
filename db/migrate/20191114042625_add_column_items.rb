cd48b1c6e32dab46d4bac02f81d316e44dc56cd9
suzuki-nori <www.rino19@gmail.com>
2019-11-14 19:28:08 +0900

    revert.commit

--- db/migrate/20191114042625_add_column_items.rb
+++
@@ -1,6 +0,0 @@
-class AddColumnItems < ActiveRecord::Migration[5.2]
-  def change
-    add_column :items, :seller_id, :integer
-    add_column :items, :buyer_id, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_14_042625) do
+ActiveRecord::Schema.define(version: 2019_11_13_114506) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "post_code"
